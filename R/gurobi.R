740cb2dcc41664f037c574fb9cfb488cd0f4f8ad
Pablo Moreno <pablo.a.moreno@gmail.com>
2015-03-16 15:59:34 +0000

    Improves generation of the gurobi model to use sparse matrices and faster visits to the A matrix being filled.

--- R/gurobi.R
+++ R/gurobi.R
@@ -17,26 +17,36 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   rowsA1<-geneAmount*ontSize
   colsA1<-rowsA1+ontSize
   
-  A1<-matrix(0,rowsA1,colsA1)
+  #A1<-matrix(0,rowsA1,colsA1)
+  #A1<-simple_triplet_zero_matrix(nrow = rowsA1, ncol = colsA1, mode = "integer")
+  A1<-Matrix(0,rowsA1,colsA1,sparse = TRUE)
   
   i<-0
   j<-1
   tj<-0
-  for(col in 1:colsA1) {
-    if(i==geneAmount) {
-      i<-1
-      j<-j+1
-    } else {
-      i<-i+1
-    }
+  # we don't need to look at all columns, only those below rowsA1
+  for(col in 1:min(colsA1,rowsA1))  {
+    # For this part is not being used.
+    #if(i==geneAmount) {
+    #  i<-1
+    #  j<-j+1
+    #} else {
+    #  i<-i+1
+    #}
     
-    for(row in 1:rowsA1) {
-      if( col <= rowsA1 ) {
-        if( row == col ) {
-          A1[row, col]<-1
-        }
-      }
-    }
+    # Since we know which row we want, we don't need to go through all of them!!!
+    #for(row in 1:rowsA1) {
+    #  if( col <= rowsA1 ) {
+    #    if( row == col ) {
+    #      A1[row, col]<-1
+    #    }
+    #  }
+    #}
+    # Instead we can do:
+    # And we can avoid the if if we only visit the required columns
+    #if(col <= rowsA1) {
+      A1[col, col]<-1
+    #}
   }
   
   ## Senses1
@@ -51,27 +61,36 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   
   rowsA2<-geneAmount
   colsA2<-colsA1
-  A2<-matrix(0,rowsA2,colsA2)
+  #A2<-matrix(0,rowsA2,colsA2)
+  #A2<-simple_triplet_zero_matrix(nrow = rowsA2, ncol = colsA2)
+  A2<-Matrix(0,rowsA2,colsA2,sparse = TRUE)
   
   i<-0
   j<-1
   tj<-0
   
-  for(col in 1:colsA2) {
+  # we only need to visit cols that are smaller than geneAmount*ontSize
+  for(col in 1:min(colsA2,geneAmount*ontSize) ) {
     if(i==geneAmount) {
       i<-1
-      j<-j+1
+      #j<-j+1 # not used here
     } else {
       i<-i+1
     }
     
-    for(row in 1:rowsA2) {
-      if( col <= geneAmount*ontSize ) {
-        if( row == i ) {
-          A2[row, col]<-1
-        }
-      }
-    }
+    # Since we know which row we want, we don't need to go through all of them!!!
+    #for(row in 1:rowsA2) {
+    #  if( col <= geneAmount*ontSize ) {
+    #    if( row == i ) {
+    #      A2[row, col]<-1
+    #    }
+    #  }
+    #}
+    # Instead we can do:
+    # if we visit the correct columns, we don't need the if
+    #if(col <= geneAmount*ontSize) {
+      A2[i,col]<-1
+    #}
   }
   
   ## senses2
@@ -86,35 +105,61 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   
   rowsA3<-ontSize
   colsA3<-colsA1
-  A3<-matrix(0,rowsA3,colsA3)
+  #A3<-matrix(0,rowsA3,colsA3)
+  #A3<-simple_triplet_zero_matrix(nrow = rowsA3, ncol = colsA3)
+  A3<-Matrix(0,rowsA3,colsA3,sparse = TRUE)
   
   i<-0
   j<-1
   tj<-0
   
-  for(col in 1:colsA3) {
-    if(col <= geneAmount*ontSize) {
-      if(i==geneAmount) {
-        i<-1
-        j<-j+1
-      } else {
-        i<-i+1
-      }
+  # we should split this for into two parts, 1 to geneAmount*ontSize and geneAmount*ontSize+1 to colsA3
+#   for(col in 1:colsA3) {
+#     if(col <= geneAmount*ontSize) {
+#       if(i==geneAmount) {
+#         i<-1
+#         j<-j+1
+#       } else {
+#         i<-i+1
+#       }
+#     } else {
+#       tj<-col-(geneAmount*ontSize)
+#     }
+#     
+#     # Since we know which row we want, we don't need to go through all of them!!!
+#     #for(row in 1:rowsA3) {
+#     #  if( col <= geneAmount*ontSize ) {
+#     #    if( row == j ) {
+#     #      A3[row, col]<-1
+#     #    }
+#     #  } else {
+#     #    if( row == tj) {
+#     #      A3[row, col]<-geneAmount*-1
+#     #    }
+#     #  }
+#     #}
+#     # Instead we can do
+#     if( col <= geneAmount*ontSize ) {
+#         A3[j, col]<-1
+#     } else {
+#         A3[tj, col]<-geneAmount*-1
+#     }
+#   }
+  
+  # so we can re-write like this:
+  for(col in 1:(geneAmount*ontSize) ) {
+    if(i==geneAmount) {
+      i<-1
+      j<-j+1
     } else {
-      tj<-col-(geneAmount*ontSize)
+      i<-i+1
     }
     
-    for(row in 1:rowsA3) {
-      if( col <= geneAmount*ontSize ) {
-        if( row == j ) {
-          A3[row, col]<-1
-        }
-      } else {
-        if( row == tj) {
-          A3[row, col]<-geneAmount*-1
-        }
-      }
-    }
+    A3[j, col]<-1
+  } 
+  for(col in (geneAmount*ontSize+1):colsA3  ) {
+    tj<-col-(geneAmount*ontSize)
+    A3[tj, col]<-geneAmount*-1
   }
   
   ## senses3
@@ -129,7 +174,9 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   
   rowsA5<-ontSize
   colsA5<-colsA1
-  A5<-matrix(0,rowsA5,colsA5)
+  #A5<-matrix(0,rowsA5,colsA5)
+  #A5<-simple_triplet_zero_matrix(nrow = rowsA5, ncol = colsA5)
+  A5<-Matrix(0,rowsA5,colsA5,sparse = TRUE)
   
   i<-0
   tj<-0
@@ -137,11 +184,14 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   for(col in (geneAmount*ontSize+1):colsA5) {
     tj<-col-(geneAmount*ontSize)
     
-    for(row in 1:rowsA5) {
-      if( row == tj) {
-        A5[row, col]<-(minDistance - d_j[tj])
-      }
-    }
+    # Since we know which row we want, we don't need to go through all of them!!!
+    #for(row in 1:rowsA5) {
+    #  if( row == tj) {
+    #    A5[row, col]<-(minDistance - d_j[tj])
+    #  }
+    #}
+    # Instead we can do:
+    A5[tj,col]<-(minDistance - d_j[tj])
   }
   
   ## senses5
@@ -156,7 +206,9 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   
   rowsA4<-1
   colsA4<-colsA1
-  A4<-matrix(0,rowsA4,colsA4)
+  #A4<-matrix(0,rowsA4,colsA4)
+  #A4<-simple_triplet_zero_matrix(nrow = rowsA4, ncol = colsA4)
+  A4<-Matrix(0,rowsA4,colsA4,sparse=TRUE)
   
   for(col in ((geneAmount*ontSize)+1):colsA4) {
     A4[1,col]<-1
@@ -173,7 +225,8 @@ produceGurobiModel<-function(annotationList,maxAnnotationTerms=20,minDistanceFro
   ## Put everything together
   
   objFun<-c(rep(d_j,times = geneAmount),rep(0,times = ontSize))
-  rbind(A1, A2, A3, A4, A5)->Amatrix
+  #rbind(A1, A2, A3, A4, A5)->Amatrix
+  rBind(A1, A2, A3, A4, A5)->Amatrix
   c(senses1,senses2,senses3,senses4,senses5)->senses
   c(b1,b2,b3,b4,b5)->bVector
   
